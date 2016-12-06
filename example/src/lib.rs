78bffb1f152dcfa47f6654fdee4e2f938fc29ac8
faineance <faineance@users.noreply.github.com>
2016-12-06 23:41:43 +0000

    better macros

--- example/src/lib.rs
+++ example/src/lib.rs
@@ -1,8 +1,4 @@
 #[macro_use]
 extern crate redismodule;
-extern crate libc;
-use std::ffi::CString;
-use redismodule::raw;
-
 
 REDIS_MODULE!("example", 1);
--- src/lib.rs
+++ src/lib.rs
@@ -4,20 +4,22 @@
 extern crate bitflags;
 extern crate libc;
 pub mod raw;
-
+pub mod command;
 #[macro_export]
 macro_rules! REDIS_MODULE (
-    ($name:expr, $module_version:expr) => (
+    ($name:expr,$module_version:expr) => (
+        use redismodule::raw;
         #[no_mangle]
         #[allow(non_snake_case)]
         pub extern "C" fn RedisModule_OnLoad(ctx: *mut raw::RedisModuleCtx) -> raw::Status {
             unsafe {
-              if raw::Export_RedisModule_Init(ctx,CString::new($name).unwrap().as_ptr(), $module_version, raw::REDISMODULE_APIVER_1) ==  raw::Status::Err {
+              if raw::Export_RedisModule_Init(ctx,format!("{}\0",$name).as_ptr() as *const i8, $module_version, raw::REDISMODULE_APIVER_1) == raw::Status::Err {
                   return raw::Status::Err;
               }
+
+              
               return raw::Status::Ok;
             }
-
         }
     )
 );
